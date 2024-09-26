16bdf4f5778c45de0eb392fe2d10958730d5793c
Rafael Petrov <djfanta@abv.bg>
2024-09-26 16:49:36 +0300

    fix: fixed pet model

--- Petstagram/pets/admin.py
+++ Petstagram/pets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from Petstagram.pets.models import Pet
+
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    pass
--- Petstagram/pets/models.py
+++ Petstagram/pets/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Pet(models.Model):
     name = models.CharField(max_length=30)
+
     personal_photo = models.URLField()
+
     date_of_birth = models.DateField(blank=True, null=True)
-    slug = models.SlugField(unique=True, null=False, blank=True)
+
+    slug = models.SlugField(null=False, blank=True, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(f"{self.name}-{self.id}")
+
+        super().save(*args, **kwargs)
