5739def4e9c8ef8ef0b112124f56e01b4aef3739
Rafael Petrov <djfanta@abv.bg>
2024-09-27 11:12:03 +0300

    fix: filepath for photos

--- Petstagram/photos/models.py
+++ Petstagram/photos/models.py
@@ -6,7 +6,7 @@ from Petstagram.photos.validators import FileSizeValidator
 
 
 class Photo(models.Model):
-    photo = models.ImageField(validators=[FileSizeValidator(5),],)
+    photo = models.ImageField(upload_to='mediafiles', validators=[FileSizeValidator(5),],)
 
     description = models.TextField(max_length=300, validators=(MinLengthValidator(10),), blank=True, null=True)
 
